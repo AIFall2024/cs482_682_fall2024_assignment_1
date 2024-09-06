bccf1f4650eb57db790886a3005bfa542882d0a9
Prateek Arora <prateekarorav2@gmail.com>
2024-09-06 03:06:09 -0700

    added test cases

---
+++ .test_q1_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from linear_regression_q1 import MyLinearRegression
+import unittest
+import numpy as np
+
+class TestMyLinearRegression(unittest.TestCase):
+
+    def test_basic_play_game_1(self):
+        classifier = MyLinearRegression('test_q1.csv')
+        acc = classifier.model_predict()
+        acc = acc*100.0
+        print("Your accuracy= {}%".format(acc))
+        self.assertTrue(acc >= 89.0)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ .test_q2_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from linear_regression_q2 import MyLinearRegression
+import unittest
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+class TestMyLinearRegression(unittest.TestCase):
+
+    def test_basic_play_game_1(self):
+        classifier = MyLinearRegression('dataset_q2.csv')
+        acc = classifier.model_predict()
+        acc = acc*100.0
+        print("Your accuracy= {}%".format(acc))
+        self.assertTrue(acc >= 1.0)
+
+if __name__ == '__main__':
+    unittest.main()
